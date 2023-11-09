2ebb58566acfe5f6fabc1a099551ec605a3e40d1
AgusT613 <torresagustin613@gmail.com>
2023-11-09 15:58:17 -0300

    Develop navigation bar structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Header from '@/components/Header'
+
 export default function Home () {
   return (
     <>
-    <h1 className='bg-red-50 p-5 text-xl text-slate-500 font-medium text-center'>Red Mascotera</h1>
+      <Header />
     </>
   )
 }
